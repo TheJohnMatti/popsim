32207848548e06f221eb58bc5c2fa97783979806
John Matti <jamatti@uwaterloo.ca>
2025-06-08 02:13:18 -0400

    started setting up sim settings

--- src/stores/useGroupStore.tsx
+++ src/stores/useGroupStore.tsx
@@ -15,4 +15,14 @@ interface GroupStore {
     groups: Group[];
 }
 
-const useGroupStore = create
+const useGroupStore = create<GroupStore>(() => ({
+    groups: [
+        {
+            color: 0x00ff00, // Default color is green
+            renderRegion: {
+                tl: { x: -10, y: -10 },
+                br: { x: 10, y: 10 }
+            }
+        }
+    ]
+}));
